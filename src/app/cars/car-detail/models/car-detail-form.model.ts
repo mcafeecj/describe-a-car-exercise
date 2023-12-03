cc6af8ed3b70132d8c395180a2d5dd3bc959406e
Chris McAfee <chrismcafee24@gmail.com>
2023-12-03 13:43:46 -0500

    updating angular to v14

--- src/app/cars/car-detail/models/car-detail-form.model.ts
+++ src/app/cars/car-detail/models/car-detail-form.model.ts
@@ -1,10 +1,27 @@
-import { FormControl } from "@angular/forms";
+import { FormControl, FormGroup } from "@angular/forms";
+
+type CarFormData = {
+  model: string;
+  year: number;
+  mileage: number;
+  exteriorColor: string;
+  interiorColor: string;
+};
 
 export class CarDetailForm {
-    make: FormControl = new FormControl();
-    model: FormControl = new FormControl();
-    year: FormControl = new FormControl();
-    mileage: FormControl = new FormControl();
-    exteriorColor: FormControl = new FormControl();
-    interiorColor: FormControl = new FormControl();
+  make: FormControl = new FormControl();
+  model: FormControl = new FormControl();
+  year: FormControl = new FormControl();
+  mileage: FormControl = new FormControl();
+  exteriorColor: FormControl = new FormControl();
+  interiorColor: FormControl = new FormControl();
+
+  formGroup: FormGroup<CarFormData> = new FormGroup<CarFormData>({
+    make: this.make,
+    model: this.model,
+    year: this.year,
+    mileage: this.mileage,
+    exteriorColor: this.exteriorColor,
+    interiorColor: this.interiorColor,
+  });
 }
